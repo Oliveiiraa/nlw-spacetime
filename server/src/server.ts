b6a8d7b9b7dc7de1e2cee708a251b05aa318ed82
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2023-05-16 22:53:09 -0300

    feat: add first route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.get('/hello', () => {
-  return 'Hello World!'
+app.get('/users', async () => {
+  return await prisma.user.findMany()
 })
 
 app
