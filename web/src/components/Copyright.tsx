3f7ff501c23d2b94da29f722691e2351117a8479
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2023-06-16 17:48:33 -0300

    feat: add components

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💙 por{" "}
+      <a
+        target="_blank"
+        href="https://www.linkedin.com/in/gabriel-h-oliveira/"
+        className="underline hover:text-gray-100"
+        rel="noreferrer"
+      >
+        Gabriel Oliveira
+      </a>
+    </div>
+  );
+}
